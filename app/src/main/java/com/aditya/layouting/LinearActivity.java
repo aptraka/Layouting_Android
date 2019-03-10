f2d64bdf77543955e0fd2912cd339bcc1edc9f08
aptraka <adity.putra14@gmail.com>
2019-03-10 22:19:04 +0700

    Create Layout with Linear Layout

---
+++ app/src/main/java/com/aditya/layouting/LinearActivity.java
@@ -0,0 +1,15 @@
+package com.aditya.layouting;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LinearActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_linear);
+
+        //TODO 1 : Buat Layout dengan ParentGroup LinearLayout pada activity_linear.xml
+    }
+}
